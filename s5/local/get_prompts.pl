dc735271c5a0a5ff388b9f05c63d2bbebef4a0ae
jjm <johnjosephmorgan@gmail.com>
2017-10-19 13:02:50 -0400

    Added local

---
+++ s5/local/get_prompts.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+# get_prompts.pl - make  a prompts file
+
+use strict;
+use warnings;
+use Carp;
+
+BEGIN {
+    @ARGV == 1 or croak "USAGE: get_prompts.pl <FOLD>
+$0 dev
+";
+}
+
+my ($fld) = @ARGV;
+
+my $tmpdir = "data/local/tmp/gp/german";
+my $o = "$tmpdir/$fld/prompts.tsv";
+my $l = "$tmpdir/$fld/lists/trl.txt";
+
+open my $L, '<', "$l" or croak "$l $!";
+
+open my $O, '+>', "$o" or croak "problems with $o  $!";
+
+while ( my $line = <$L> ) {
+    chomp $line;
+    open my $T, '<', $line or croak "problems with $line $!";
+    my $spkr = "";
+    my $sn = 0;
+    LINE: while ( my $linea = <$T> ) {
+	chomp $linea;
+	next LINE if ( $linea =~ /^$/);
+
+	if ( $linea =~ /^\;SprecherID\s(\d{1,3})/ ) {
+	    $spkr = $1;
+	} elsif ( $linea =~ /^\;\s(\d{1,})/ ) {
+	    my $n = $1;
+	    print $O "GE${spkr}_${n}.adc\t";
+	} else {
+	    print $O "$linea\n";
+	}      
+    }
+    close $T;
+}
+close $O;
---
+++ s5/local/prepare_lm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2017 John Morgan
+# Apache 2.0.
+
+. ./cmd.sh
+set -e
+. ./path.sh
+stage=0
+
+. ./utils/parse_options.sh
+
+if [ ! -d data/local/lm ]; then
+    mkdir -p data/local/lm
+fi
+
+# use only training prompts
+cut -f 2 data/train/text > data/local/lm/training_text.txt
+corpus=data/local/lm/training_text.txt
+
+ngram-count \
+    -order 3 \
+    -interpolate \
+    -unk \
+    -map-unk "<UNK>" \
+    -limit-vocab \
+    -text $corpus \
+    -lm data/local/lm/threegram.arpa || exit 1;
+
+if [ -e "data/local/lm/threegram.arpa.gz" ]; then
+    rm data/local/lm/threegram.arpa.gz
+fi
+
+gzip \
+    data/local/lm/threegram.arpa
