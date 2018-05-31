7a4fede6ce7834ef2b65359eaee6a1576ea1eb4a
John Joseph Morgan <johnjosephmorgan@gmail.com>
2018-05-31 14:12:48 -0400

    Simplified.

--- s5/run.sh
+++ s5/run.sh
@@ -36,8 +36,8 @@ gp_lm=http://www.csl.uni-bremen.de/GlobalPhone/lm/GE.3gram.lm.gz
 tmpdir=data/local/tmp/gp/german
 
 # The number of jobs you can run will depend on the system
-decoding_jobs=5
-training_jobs=56
+decoding_jobs=5n
+nj=56
 
 # global phone data prep
 if [ $stage -le 0 ]; then
@@ -120,7 +120,7 @@ if [ $stage -le 2 ]; then
     # G.fst will be generated in a later step.
     utils/prepare_lang.sh \
 	--position-dependent-phones true data/local/dict "<UNK>" \
-	data/local/lang_tmp data/lang
+	$tmpdir/lang_tmp $tmpdir/lang
 fi
 
 if [ $stage -le 3 ]; then
@@ -136,14 +136,14 @@ if [ $stage -le 3 ]; then
     #local/prepare_lm.sh
 
     # Now generate the G.fst file from the lm.
-    # Notice that it will be stored under data/lang_test.
     utils/format_lm.sh \
-	data/lang data/local/lm/threegram.arpa.gz data/local/dict/lexicon.txt \
-	data/lang_test
+	$tmpdir/lang data/local/lm/threegram.arpa.gz data/local/dict/lexicon.txt \
+	data/lang
 fi
 
 if [ $stage -le 4 ]; then
-    # Create ConstArpaLm format language model for full 3-gram and 4-gram LMs
+    # Create ConstArpaLm format language model
+    # Notice that it ends up under data/lang_test
     utils/build_const_arpa_lm.sh \
 	data/local/lm/threegram.arpa.gz \
 	data/lang \
@@ -156,9 +156,7 @@ if [ $stage -le 5 ]; then
     # The feature files will be stored under plp_pitch
     # plp and pitch features are extracted.
     for fld in dev eval train ; do
-	steps/make_plp_pitch.sh \
-	    --cmd run.pl --nj $training_jobs data/$fld exp/make_plp_pitch/$fld \
-	    plp_pitch
+	steps/make_plp_pitch.sh data/$fld exp/make_plp_pitch/$fld plp_pitch
 
 	utils/fix_data_dir.sh data/$fld
 
@@ -169,174 +167,99 @@ if [ $stage -le 5 ]; then
 fi
 
 if [ $stage -le 6 ]; then
+    # This is the first of several acoustic model training steps .
+    # Context independent phones are trained.
     echo "Starting  monophone training in exp/mono on" `date`
-    steps/train_mono.sh \
-	--nj $training_jobs \
-	--cmd run.pl \
-	data/train \
-	data/lang \
-	exp/mono || exit 1;
+    steps/train_mono.sh data/train data/lang exp/mono
 fi
 
 if [ $stage -le 7 ]; then
-    # align with monophones
-    steps/align_si.sh \
-	--nj $training_jobs \
-	--cmd run.pl \
-	data/train \
-	data/lang \
-	exp/mono \
-	exp/mono_ali || exit 1;
+    # This step uses the monophones just trained to time align the data
+    steps/align_si.sh data/train data/lang exp/mono exp/mono_ali
 fi
 
 if [ $stage -le 8 ]; then
-    mkdir -p exp/mono/graph
+    # Test the monophone models.
     (
-	utils/mkgraph.sh \
-	data/lang_test \
-	exp/mono \
-	exp/mono/graph
+	# A graph is required for decoding.
+	utils/mkgraph.sh data/lang_test exp/mono exp/mono/graph
 
 	for fld in dev eval; do
-	    steps/decode.sh \
-		--nj $decoding_jobs \
-		--cmd "$decode_cmd" \
-		exp/mono/graph \
-		data/$fld \
-		exp/mono/decode_${fld}
+	    # The following command does speech recognition using the monophone models 
+	    steps/decode.sh exp/mono/graph data/$fld exp/mono/decode_${fld}
 	done
     ) &
+    # Testing can be run in the background
 fi
 
 if [ $stage -le 9 ]; then
+    # This is the first step  for training context dependent acoustic models
     echo "Starting  triphone training in exp/tri1 on" `date`
     steps/train_deltas.sh \
-	--cluster-thresh 100 \
-	--cmd run.pl \
-	3100 \
-	50000 \
-	data/train \
-	data/lang \
-	exp/mono_ali \
-	exp/tri1 || exit 1;
+	--cluster-thresh 100 3100 50000 data/train data/lang exp/mono_ali \
+	exp/tri1
 fi
 
 if [ $stage -le 10 ]; then
     # align with triphones
-    steps/align_si.sh \
-	--nj $training_jobs \
-	--cmd run.pl \
-	data/train \
-	data/lang \
-	exp/tri1 \
-	exp/tri1_ali
+    steps/align_si.sh data/train data/lang exp/tri1 exp/tri1_ali
 fi
 
 if [ $stage -le 11 ]; then
-    mkdir -p exp/tri1/graph
-
+    # Test the triphone models.
     (
-	utils/mkgraph.sh \
-	    data/lang_test \
-	    exp/tri1 \
-	    exp/tri1/graph
+	utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph
 
 	for fld in dev eval; do
-	    steps/decode.sh \
-		--nj $decoding_jobs \
-		--cmd "$decode_cmd" \
-		exp/tri1/graph \
-		data/$fld \
-		exp/tri1/decode_${fld}
+	    steps/decode.sh exp/tri1/graph data/$fld exp/tri1/decode_${fld}
 	done
     ) &
 fi
 
 if [ $stage -le 12 ]; then
+    # Trains with front end feature adaptation.
     echo "Starting (lda_mllt) triphone training in exp/tri2b on" `date`
     steps/train_lda_mllt.sh \
 	--splice-opts "--left-context=3 --right-context=3" \
-	3100 \
-	50000 \
-	data/train \
-	data/lang \
-	exp/tri1_ali \
-	exp/tri2b
+	3100 50000 data/train data/lang exp/tri1_ali exp/tri2b
 fi
 
 if [ $stage -le 13 ]; then
     # align with lda and mllt adapted triphones
     steps/align_si.sh \
-	--use-graphs true \
-	--nj $training_jobs \
-	--cmd run.pl \
-	data/train \
-	data/lang \
-	exp/tri2b \
-	exp/tri2b_ali
+	--use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali
 fi
 
 if [ $stage -le 14 ]; then
     # Decode tri2b
-    mkdir -p exp/tri2b/graph
-
     (
-	utils/mkgraph.sh \
-	    data/lang_test \
-	    exp/tri2b \
-	    exp/tri2b/graph
+	utils/mkgraph.sh data/lang_test exp/tri2b exp/tri2b/graph
 
 	for fld in dev eval; do
-	    steps/decode.sh \
-		--nj $decoding_jobs \
-		--cmd "$decode_cmd" \
-		exp/tri2b/graph \
-		data/$fld \
-		exp/tri2b/decode_${fld}
+	    steps/decode.sh exp/tri2b/graph data/$fld exp/tri2b/decode_${fld}
 	done
     ) &
 fi
 
 if [ $stage -le 15 ]; then
+    # Models are speaker adapted.
         echo "Starting (SAT) triphone training in exp/tri3b on" `date`
-    steps/train_sat.sh \
-	--cmd run.pl \
-	3100 \
-	50000 \
-	data/train \
-	data/lang \
-	exp/tri2b_ali \
-	exp/tri3b
+    steps/train_sat.sh 3100 50000 data/train data/lang exp/tri2b_ali exp/tri3b
 fi
 
 if [ $stage -le 16 ]; then
     echo "Starting exp/tri3b_ali on" `date`
-    steps/align_fmllr.sh \
-	--nj $training_jobs \
-	--cmd run.pl \
-	data/train \
-	data/lang \
-	exp/tri3b \
-	exp/tri3b_ali
+    steps/align_fmllr.sh data/train data/lang exp/tri3b exp/tri3b_ali
 fi
 
 if [ $stage -le 17 ]; then
     # Decode tri3b
-    mkdir -p exp/tri3b/graph
-
     (
-	utils/mkgraph.sh \
-	    data/lang_test \
-	    exp/tri3b \
-	    exp/tri3b/graph
+	utils/mkgraph.sh data/lang_test exp/tri3b exp/tri3b/graph
 
 	for fld in dev eval; do
 	    steps/decode_fmllr.sh \
-		--nj $decoding_jobs \
-		--cmd "$decode_cmd" \
-		exp/tri3b/graph \
-		data/$fld \
-		exp/tri3b/decode_${fld}
+		exp/tri3b/graph data/$fld exp/tri3b/decode_${fld}
 	done
     ) &
 fi
