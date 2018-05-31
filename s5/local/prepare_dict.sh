69f477f7d6f7424cd45d833e0544b36a25c43f1f
John Joseph Morgan <johnjosephmorgan@gmail.com>
2018-05-31 13:07:43 -0400

    Using variables.

--- s5/local/prepare_dict.sh
+++ s5/local/prepare_dict.sh
@@ -7,26 +7,29 @@ set -o errexit
 
 [ -f ./path.sh ] && . ./path.sh
 
+# Assumes the normalized lexicon exists in the following file
+d=data/local/tmp/gp/german/dict/lexicon.txt
 # The dictionary will end up under data/local/
-if [ ! -d data/local/dict ]; then
-    mkdir -p data/local/dict
+o=data/local/dict
+if [ ! -d $o ]; then
+    mkdir -p $o
 fi
 
 export LC_ALL=C
 
 # get the phones from the dictionary
 cut \
-    -f2- -d "	" data/local/tmp/gp/german/dict/lexicon.txt | \
+    -f2- -d "	" $d | \
     tr -s '[:space:]' '[\n*]' | grep -v SPN | sort -u > \
-    data/local/dict/nonsilence_phones.txt
+    $o/nonsilence_phones.txt
 
 # remove tabs
 expand \
-    data/local/tmp/gp/german/dict/lexicon.txt | sort -u | sed "1d" > \
-    data/local/dict/lexicon.txt
+    $d | sort -u | sed "1d" > \
+    $o/lexicon.txt
 
 # append an entry for the unknown symbol
-echo "<UNK>	SPN" >> data/local/dict/lexicon.txt
+echo "<UNK>	SPN" >> $o/lexicon.txt
 
 # silence phones, one per line.
 {
@@ -34,18 +37,18 @@ echo "<UNK>	SPN" >> data/local/dict/lexicon.txt
     echo SPN;
 } \
     > \
-    data/local/dict/silence_phones.txt
+    $o/silence_phones.txt
 
 echo \
     SIL \
     > \
-    data/local/dict/optional_silence.txt
+    $o/optional_silence.txt
 
 (
-    tr '\n' ' ' < data/local/dict/silence_phones.txt;
+    tr '\n' ' ' < $o/silence_phones.txt;
     echo;
-    tr '\n' ' ' < data/local/dict/nonsilence_phones.txt;
+    tr '\n' ' ' < $o/nonsilence_phones.txt;
     echo;
-) >data/local/dict/extra_questions.txt
+) >$o/extra_questions.txt
 
 echo "Finished dictionary preparation."
